da23350a2a270b1e72145c82e98c21ad462f62d7
nemutas <inohiro.focru@gmail.com>
2023-09-18 01:34:03 +0900

    😑

--- src/scripts/webgl/shader/entitiesFs.glsl
+++ src/scripts/webgl/shader/entitiesFs.glsl
@@ -1,6 +1,8 @@
 precision highp float;
 
 void main(){
+  if (0.5 < distance(gl_PointCoord.xy, vec2(0.5))) discard;
+
   float d = 1.0 - length(gl_PointCoord.xy - 0.5);
   gl_FragColor = vec4(d, 0.0, 0.0, 1.0);
 }
--- src/scripts/webgl/shader/entitiesVs.glsl
+++ src/scripts/webgl/shader/entitiesVs.glsl
@@ -7,5 +7,5 @@ void main(){
     sim = sim * 2.0 - 1.0;
 
     gl_Position = vec4(sim, 0.0, 1.0);
-    gl_PointSize = 0.5;
+    gl_PointSize = 1.0;
 }
